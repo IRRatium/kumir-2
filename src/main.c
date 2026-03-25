40b303b69135b9bcd81ac371abb38ec4b002069a
IRRing <majnkraftiigry7@gmail.com>
2026-03-25 22:13:48 +0300

    upd

--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 #endif
+#endif
+
+// Глобальная переменная для файлов
+const char* current_filename = "main";
 
 char* read_file_content(const char* filename) {
     FILE* file = NULL;
@@ -26,13 +32,21 @@ int main(int argc, char** argv) {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+    // ВКЛЮЧАЕМ ПОДДЕРЖКУ ANSI ЦВЕТОВ В КОНСОЛИ WINDOWS
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
 #endif
 
-    if (argc < 2) { printf("Использование: kumir2 <файл.kum>\n"); return 1; }
-    if (!strstr(argv[1], ".kum")) { printf("Ошибка: Файл должен иметь расширение .kum\n"); return 1; }
+    if (argc < 2) { printf("\033[1;36mИспользование: kumir2 <файл.kum>\033[0m\n"); return 1; }
+    if (!strstr(argv[1], ".kum")) { printf("\033[1;31mОшибка: Файл должен иметь расширение .kum\033[0m\n"); return 1; }
+
+    current_filename = argv[1]; // СОХРАНЯЕМ ИМЯ ЗАПУСКАЕМОГО ФАЙЛА
 
     char* source = read_file_content(argv[1]);
-    if (!source) { printf("Ошибка: не удалось прочитать главный файл %s\n", argv[1]); return 1; }
+    if (!source) { printf("\033[1;31mОшибка: не удалось прочитать главный файл %s\033[0m\n", argv[1]); return 1; }
     
     ASTNode* ast = parse(source);
     execute(ast);
